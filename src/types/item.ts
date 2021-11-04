cc99df87a7207698364f4e057b9a9e4dc38a263e
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-04 11:54:54 +0900

    演習1のVueファイルを作成＆Itemクラスを作成。

---
+++ src/types/item.ts
@@ -0,0 +1,19 @@
+export class Item {
+  constructor(private _id: number, private _name: string) {}
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+}
